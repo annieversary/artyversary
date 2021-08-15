71b1322869c04ed33e52be6ab0e7336f2a498046
annieversary <annnnnie@protonmail.com>
2021-08-15 23:32:45 +0200

    added a template crate

---
+++ crates/template/src/main.rs
@@ -0,0 +1,22 @@
+use nannou::prelude::*;
+
+fn main() {
+    nannou::app(model).update(update).simple_window(view).run();
+}
+
+struct Model {}
+
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
+fn view(app: &App, _model: &Model, frame: Frame) {
+    let _t = frame.nth() as f32 / 60.0;
+
+    let draw = app.draw();
+    draw.background().color(PLUM);
+    draw.ellipse().color(STEELBLUE);
+    draw.to_frame(app, &frame).unwrap();
+}
